4dc3b41907bbc53875280abd0153f236d9bccce9
Kenneth Farrell <kenneth.farrell@waystar.com>
2020-07-16 11:24:50 -0400

    booleans and condition evaluation

--- lambdaCalculus.js
+++ lambdaCalculus.js
@@ -24,4 +24,23 @@
 // functionPassing = firstIdentity(secondIdentity);
 // valuePassing = functionPassing(2);
 
+/** Booleans and Conditional Statements
+ * True:  lambda x . lambda y . x
+ * False:  lambda x . lambda y . y
+ * Condition Evaluation:  lambda x . lambda y . lambda z . x y z
+*/
+
+True = (x => y => x);
+False = (x => y => y);
+
+// trueFirstApply = True(true);
+// trueSecondApply = trueFirstApply(false);
+
+// trueFirstApply = True(false);
+// trueSecondApply = trueFirstApply(true);
+
+If = (x => y => z => x(y)(z));
+shouldReturnTrue = If(True)("TRUE")("oops");
+shouldReturnFalse = If(False)("oops")("FALSE");
+
 console.log();
